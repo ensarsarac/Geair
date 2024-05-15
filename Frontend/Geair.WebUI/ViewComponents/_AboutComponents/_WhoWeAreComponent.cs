aec1ac3ce6ff0841b5c5da27fceafe5d42555a49
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-05-15 13:09:05 +0300

    Hakkımızda sayfası consume edildi.

--- Frontend/Geair.WebUI/ViewComponents/_AboutComponents/_WhoWeAreComponent.cs
+++ Frontend/Geair.WebUI/ViewComponents/_AboutComponents/_WhoWeAreComponent.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Geair.DTOLayer.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Geair.WebUI.ViewComponents._AboutComponents
 {
     public class _WhoWeAreComponent:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _WhoWeAreComponent(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync("https://localhost:7151/api/Abouts");
+            var readData = await res.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(readData);
+            return View(values.FirstOrDefault());
         }
     }
 }
