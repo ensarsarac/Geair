c79bd171bc66b49bccfb31bfb8e20b016f6d8d04
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-05-25 17:38:56 +0300

    Proje son hali güncel.

--- Frontend/Geair.WebUI/ViewComponents/_UILayoutComponents/_UILayoutNavbarComponent.cs
+++ Frontend/Geair.WebUI/ViewComponents/_UILayoutComponents/_UILayoutNavbarComponent.cs
@@ -4,8 +4,16 @@ namespace Geair.WebUI.ViewComponents._UILayoutComponents
 {
     public class _UILayoutNavbarComponent:ViewComponent
     {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public _UILayoutNavbarComponent(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.user = _contextAccessor.HttpContext.User.FindFirst("fullname").Value;
             return View();
         }
     }
