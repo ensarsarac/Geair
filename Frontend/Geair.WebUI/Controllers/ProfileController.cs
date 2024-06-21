35cb9fa421ca5b124fc9fde90316725bbbf35841
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-06-21 11:01:47 +0300

    Kullanıcılar için profile sayfası oluşturuldu.

---
+++ Frontend/Geair.WebUI/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Geair.WebUI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
